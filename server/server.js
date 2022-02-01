d15fdd6dc964ef2a4bb335aa614c6e623bf2dfd3
Andy Swanson <swan1625@icloud.com>
2022-02-01 16:30:06 -0600

    end of notes

--- server/server.js
+++ server/server.js
@@ -14,6 +14,33 @@ const port = 5000;
 // folder name is server/public
 app.use(express.static('server/public'));
 
+
+// let quoteList = [
+//     { text: 'I\'m not going to school just for the academics - I wanted to share ideas, to be around people who are passionate about learning.', author: 'Emma Watson' },
+//     { text: 'Remember there\'s no such thing as a small act of kindness. Every act creates a ripple with no logical end.', author: 'Scott Adams' },
+//     { text: 'Intelligence plus character-that is the goal of true education.', author: 'Martin Luther King, Jr.' }
+//   ];
+
+const quoteList = require('./modules/quoteList'); //dont need to add .js on quotelist because JS is sloppy
+
+
+// when we visit localhost:5000/quotes in our browser, express will call this function 
+//req is request. res is response
+  app.get('/quotes', function(req, res){
+    console.log('Request at /quotes was made', req);
+    res.send(quoteList);
+    // res.sendStatus(500); // sends internal server error
+
+  });
+
+
+// 4 common methods
+// .get
+// .post
+// .push
+// .delete
+
+
 // start up our server
 app.listen(port, function() {
     console.log('listening on port', port);
